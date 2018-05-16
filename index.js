a0500793eae3bb0a0fe538cc7c7cda63469b1f75
jmcacioppo <jmcacioppo0531@gmail.com>
2018-05-15 23:42:40 -0400

    Using typescript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+"use strict";
+exports.__esModule = true;
 var arr = [1, 2, 3, 4, 5];
-
 function sum(a, b) {
     return a + b;
 }
-
-module.exports = sum;
+exports.sum = sum;
---
+++ index.ts
@@ -0,0 +1,5 @@
+var arr = [1, 2, 3, 4, 5];
+
+export function sum(a, b) {
+    return a + b;
+}
